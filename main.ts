184b172659b33bf90ac44ade83339c76fbce041f
Quinnto1 <72207556+Quinnto1@users.noreply.github.com>
2020-10-01 10:06:08 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+for (let index = 0; index < 4; index++) {
+    music.playMelody("E B C5 A B G A F ", 120)
+}
+basic.showString("Hello and welcome to truth or false have fun:)")
+if (true) {
+    basic.showString("DO nothing")
+}
+if (false) {
+    basic.showString("Do 10 sit ups on camera")
+}
+basic.showString("Thomas Edison was born on February 11, 1847")
+basic.showString("When was Thomas Edison born?")
